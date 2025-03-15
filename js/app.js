ec094c36dc18af29740952606c9d13a03b8b9e67
Camilla Binni <milla.binni@gmail.com>
2025-03-15 00:50:13 -0300

    Enhance layout and functionality: update header styles, add new service and banner sections, and implement multiple sliders for improved user experience

--- js/app.js
+++ js/app.js
@@ -17,12 +17,72 @@ function menuMobile() {
 }
 
 var swiperHome = new Swiper(".slider-home", {
-    pagination: {
-        el: ".swiper-pagination-slider-home",
-        clickable: true,
-    },
     navigation: {
         nextEl: ".swiper-button-next-slider-home",
         prevEl: ".swiper-button-prev-slider-home",
+    },
+});
+
+var swiperGeneral = new Swiper(".slider-general", {
+    navigation: {
+        nextEl: ".swiper-button-next-slider-general",
+        prevEl: ".swiper-button-prev-slider-general",
+    },
+});
+
+var swiperTopExcorts = new Swiper(".slider-top-excorts", {
+    navigation: {
+        nextEl: ".swiper-button-next-top-excorts",
+        prevEl: ".swiper-button-prev-top-excorts",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
+
+var swiperBlog = new Swiper(".slider-blog", {
+    navigation: {
+        nextEl: ".swiper-button-next-blog",
+        prevEl: ".swiper-button-prev-blog",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
+
+var swiperWhy = new Swiper(".slider-why", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination-why",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            effect: "fade",
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        991: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
     },
 });
